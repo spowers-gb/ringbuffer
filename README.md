ed15077e556126af61560cdea90f80718b63c599
Scott Powers <spowers@gnubio.com>
2016-03-10 10:14:14 -0500

    simple ring buffer implementation

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ring Buffer
+
+## Introduction
+This is a simple ring buffer implementation intended to handle reading and
+writing at different locations.  Additionally the implementation supports reading
+out the entire state of the buffer without changing the location of the read
+pointer.  
---
+++ src/RingBuffer.jl
@@ -0,0 +1,39 @@
+module RingBuffer
+
+  type Ring{T}
+    memory::Array
+    read_idx::Int
+    write_idx::Int
+    size::Int
+
+    function Ring(size::Int)
+      new(Array(T, size), 1, 1, size)
+    end
+  end
+
+
+  function write!(buffer, value)
+    buffer.memory[buffer.write_idx] = value;
+    buffer.write_idx += 1;
+  end
+
+  function read(buffer)
+    value = buffer.memory[buffer.read_idx]
+    buffer.read_idx = next_index(buffer.read_idx, buffer.size)
+    return value
+  end
+
+
+  function read_all(buffer)
+    buffer.memory
+  end
+
+  function next_index(current, size)
+    next = current + 1
+    if next%size == 0
+      next = 1
+    end
+    return next
+  end
+
+end
